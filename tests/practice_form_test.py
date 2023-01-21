cb0b4bd03d37e510512449010e13850354bd8a35
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-01-21 16:38:04 +0300

    добавил allure

--- tests/practice_form_test.py
+++ tests/practice_form_test.py
@@ -1,9 +1,12 @@
+import allure
 from demoqa_tests.model.pages.data import user
 from demoqa_tests.model.pages.practice_form import PracticePage
 
 
 def test_practice():
-    john = PracticePage(user)
-    john.fill().submit()
-    john.assert_fields()
-
+    with allure.step('определяет данные для заполнения'):
+        john = PracticePage(user)
+    with allure.step('Заполняет форму'):
+        john.fill().submit()
+    with allure.step('проверяет заполненные данные'):
+        john.assert_fields()
