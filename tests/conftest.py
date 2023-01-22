fa430b9cd792667c4622eab262e4b6f4d4c878ab
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-01-23 00:03:51 +0300

    добавил связь с selenoid, читку env и видео, также пришлось поменять размер из за рекламы

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,46 @@
+import os
+
 import pytest
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+from demoqa_tests.utils import attach
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
 
-    browser.config.window_width = 1400
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
+
+    browser.config.window_width = 400
     browser.config.window_height = 800
     browser.config.base_url = 'https://demoqa.com'
-
     yield
 
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()
