f3aae23cd15892b227fd7f5cd734ccf6a8d74701
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-01-22 15:02:56 +0300

    переделываем все

--- tests/practice_form_test.py
+++ tests/practice_form_test.py
@@ -1,7 +1,8 @@
 import allure
 from demoqa_tests.model.pages.data import user
 from demoqa_tests.model.pages.practice_form import PracticePage
-
+from demoqa_tests.utils import attach
+from selene.support.shared import browser
 
 def test_practice():
     with allure.step('определяет данные для заполнения'):
@@ -10,3 +11,7 @@ def test_practice():
         john.fill().submit()
     with allure.step('проверяет заполненные данные'):
         john.assert_fields()
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
