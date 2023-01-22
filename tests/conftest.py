944c255ac6b5fd6366f25985880741ad066857c8
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-01-22 15:11:21 +0300

    добавил chromeDriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from selene.support.shared import browser
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
+    browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
     browser.config.window_width = 1400
     browser.config.window_height = 800
     browser.config.base_url = 'https://demoqa.com'
